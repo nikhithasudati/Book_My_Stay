1b03a009728e9a78cb437969dd7dfb445ba8148d
Nikhitha Sudati <nikhithasudati78@gmail.com>
2023-07-24 13:35:45 -0400

    Registration Details stored in Mongodb
    Registration details are stored in Mongodb atlas. If email is already registered, throws an error to login

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose");
+const User = require('./models/User.js');
+const bcrypt = require('bcryptjs');
+require('dotenv').config()
+
 const app = express();
 app.use(express.json());
 
+
+const bcryptSalt = bcrypt.genSaltSync(8);
 app.use(cors({
     credentials :true,
     origin : 'http://127.0.0.1:5173',
@@ -12,8 +19,23 @@ app.get('/test',(req,res) => {
     res.json('te')
 });
 
-app.post('/register',(req,res) => {
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.post('/register',async(req,res) => {
     const {name,email,password} = req.body;
-    res.json({name,email,password});
+    try{
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password,bcryptSalt),
+        });
+        res.json(userDoc);
+
+    }
+    catch(e){
+        res.status(422).json(e);
+    }
+   
 });
 app.listen(3000);
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -6,14 +6,21 @@ export default function RegisterPage() {
   const [name,setName] = useState('');
   const [email,setEmail] = useState('');
   const [password,setPassword] = useState('');
-    function registerUser(ev) {
+   async function registerUser(ev) {
     ev.preventDefault();
-
-    axios.post('/register',{
-        name,
-        email,
-        password,
-    });
+        try{
+            await axios.post('/register',{
+            name,
+            email,
+            password,
+        });
+        alert('Registration Successful. You can login now')
+        }
+        catch(e){
+            alert('Email is already in use, Login using the email')
+        }
+    
+    
   }
   return (
     <div className="mt-4 grow flex items-center justify-around">
@@ -41,3 +48,7 @@ export default function RegisterPage() {
     </div>
   );
 }
+
+
+// 2Ep3BP12jpR87bzn
+// sudatinikhitha
