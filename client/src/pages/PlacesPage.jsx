853a92ffeada558a10b7e6f1fd205e08b47a9c3e
Nikhitha Sudati <nikhithasudati78@gmail.com>
2023-08-07 12:58:22 -0400

    Code Refactoring
    Making multiple components for photos

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -2,6 +2,7 @@ import {Link,useParams} from 'react-router-dom';
 import Perks from '../Perks.jsx';
 import { useState } from 'react';
 import  axios  from 'axios';
+import PhotosUploader from '../PhotosUploader.jsx';
 
 export default function PlacesPage(){
     const {action} = useParams();
@@ -9,7 +10,6 @@ export default function PlacesPage(){
         const [title,setTitle] = useState('');
         const [address,setAddress] = useState('');
         const [addedPhotos,setAddedPhotos] = useState([]);
-        const [photoLink,setPhotoLink] = useState('');
         const [description,setDescription] = useState('');
         const [perks,setPerks] = useState([]);
         const [extraInfo,setExtraInfo] = useState('');
@@ -34,33 +34,7 @@ export default function PlacesPage(){
               </>
             );
           }
-          async function addPhotoByLink(ev){
-            ev.preventDefault();
-            const {data:filename} = await axios.post('/upload-by-link',{link: photoLink });
-            setAddedPhotos(prev =>{
-                return [...prev,filename]
-            });
-            setPhotoLink('');
-          }
-           function uploadPhoto(ev){
-            const files = ev.target.files;
-            const data = new FormData();
-            for(let i=0;i<files.length;i++){
-                data.append('photos',files[i]);
-
-            }
-
-            axios.post('/upload',data,{
-                headers :{'Content-type':'multipart/form-data'}
-            }).then(response =>{
-                const{data:filenames} = response;
-                setAddedPhotos(prev => {
-                    return[...prev,...filenames]
-                });
-                
-            })
-        }
-
+          
     return(
         <div>
             {action != 'new' && (
@@ -83,24 +57,9 @@ export default function PlacesPage(){
 
                         <input type = "text" value = {address} onChange = {ev => setAddress(ev.target.value)}  placeholder='Address' />
                         {preInput('Photos','more = better')}
+                        <PhotosUploader  addedPhotos={addedPhotos} onChange={setAddedPhotos}/>
 
-                        <div className='flex gap-2'>
-                             <input type = "text" value = {photoLink} onChange = {ev => setPhotoLink(ev.target.value)} placeholder='add using a link' />
-                                <button onClick = {addPhotoByLink} className='bg-gray-200 px-4 rounded-2xl'> Add photo</button>
-                        </div>
-                        <div className='mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
-                            {addedPhotos.length > 0 && addedPhotos.map(link => (
-                                <div key={link} className='h-32 flex'>
-                                    <img className = "rounded-2xl w-full object-cover "src={'http://localhost:3000/uploads/'+link} alt="" />
-                                </div>
-                            ))}
-                            <label className='h-32 cursor-pointer flex gap-1 gap-1 items-center justify-center border bg-transparent rounded-2xl p-2 text-3xl text-gray-600'>
-                                <input type = "file" multiple className='hidden' onChange={uploadPhoto}/>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
-                                </svg>upload
-                            </label>
-                        </div>        
+                        
                         {preInput('Description','description of the place')}
                         <textarea  value = {description} onChange = {ev => setDescription(ev.target.value)} />
                         {preInput('Perks','select all the perks of your place')}
