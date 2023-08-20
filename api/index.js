bcd3541319faea4a3c4253dbb3dfb43ace117513
Nikhitha Sudati <nikhithasudati78@gmail.com>
2023-08-20 17:13:56 -0400

    Added Place Page
    Place page to display all the details related to specific place

--- api/index.js
+++ api/index.js
@@ -133,16 +133,6 @@ app.post('/places',(req,res)=>{
     });
 });
 
-// app.get('/places',(req,res) => {
-//     const token = req.cookies;
-//     jwt.verify(token,jwtSecret,{},async(err,userData) => {
-//         const {id} = userData;
-//         res.json(await Place.find({owner:id}));
-   
-//        });
-       
-//     });
-
 
 app.get('/user-places', async (req, res) => {
     const { token } = req.cookies;
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,68 +1,3 @@
-// import {useEffect, useState} from "react";
-// import axios from "axios";
-// import {Link} from "react-router-dom";
-// // import Image from "../Image.jsx";
-
-// export default function IndexPage() {
-//   const [places,setPlaces] = useState([]);
-//   useEffect(() => {
-//     axios.get('/places').then(response => {
-//       setPlaces([...response.data,...response.data,...response.data]);
-//     });
-//   }, []);
-//   return (
-//     <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
-//       {places.length > 0 && places.map(place => (
-//         // <Link to={'/place/'+place._id}>
-//           <div className="bg-gray-500 mb-2 rounded-2xl flex">
-//             {place.photos?.[0] && (
-//               <img className="rounded-2xl object-cover aspect-square" src={'http://localhost:3000/uploads/'+place.photos?.[0]} alt=""/>
-//             )}
-//           </div>
-//           <h2 className="font-bold">{place.address}</h2>
-//           <h3 className="text-sm text-gray-500">{place.title}</h3>
-//           <div className="mt-1">
-//             <span className="font-bold">${place.price}</span> per night
-//           </div>
-//         </Link>
-//       ))}
-//     </div>
-//   );
-// }
-
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-// import { Link } from "react-router-dom";
-
-// export default function IndexPage() {
-//   const [places, setPlaces] = useState([]);
-  
-//   useEffect(() => {
-//     axios.get('/places').then(response => {
-//       setPlaces(response.data);
-//     });
-//   }, []);
-  
-//   return (
-//     <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
-//       {places.length > 0 && places.map(place => (
-//         <Link to={'/place/'+place._id} key={place._id}>
-//           <div className="bg-gray-500 mb-2 rounded-2xl flex flex-col">
-//             {place.photos?.[0] && (
-//               <img className="rounded-2xl object-cover aspect-square" src={'http://localhost:3000/uploads/'+place.photos?.[0]} alt=""/>
-//             )}
-//             <h2 className="font-bold">{place.address}</h2>
-//             <h3 className="text-sm text-gray-500">{place.title}</h3>
-//             <div className="mt-1">
-//               <span className="font-bold">${place.price}</span> per night
-//             </div>
-//           </div>
-//         </Link>
-//       ))}
-//     </div>
-//   );
-// }
-
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
@@ -79,8 +14,8 @@ export default function IndexPage() {
   return (
     <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
       {places.length > 0 && places.map(place => (
-        // <Link to={'/place/'+place._id} key={place._id}>
-            <div>
+         <Link to={'/place/'+place._id} key={place._id}>
+        
           <div className="bg-gray-500 mb-2 rounded-2xl flex flex-col">
             {place.photos?.[0] && (
               <img className="rounded-2xl object-cover aspect-square" src={'http://localhost:3000/uploads/'+place.photos?.[0]} alt=""/>
@@ -91,8 +26,8 @@ export default function IndexPage() {
             <div className="mt-1">
               <span className="font-bold">${place.price}</span> per night
             </div>
-        {/* </Link> */}
-        </div>
+        </Link> 
+       
       ))}
     </div>
   );
