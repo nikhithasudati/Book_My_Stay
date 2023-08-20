426efcd820b168fa986fca6c46a100228512303c
Nikhitha Sudati <nikhithasudati78@gmail.com>
2023-08-20 17:19:54 -0400

    Added date-fns library

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
       <Route path = "/account/places" element = {<PlacesPage />} />
       <Route path = "/account/places/new" element = {<PlacesFormPage />} />
       <Route path = "/account/places/:id" element = {<PlacesFormPage />} />
-      <Route path = "/places/:id" element = {<PlacePage />}/>
+      <Route path = "/place/:id" element = {<PlacePage />}/>
 
       
 
