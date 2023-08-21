d1f09b57a3aec256389227e49e11405bd7ce1805
Nikhitha Sudati <nikhithasudati78@gmail.com>
2023-08-21 12:58:57 -0400

    Final styling changes
    Styling change for place page

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -29,19 +29,24 @@ export default function PlacesPage(){
                   <div className="mt-4">
                   {places.length > 0 && places.map(place => (
                 <Link key={place._id} to={'/account/places/'+place._id} className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl">
-                  <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
+                   <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
                     {place.photos.length > 0 && (
                       <PlaceImg place ={place} />
-                      
                     )}
                   </div>
                   <div className="grow-0 shrink">
                     <h2 className="text-xl">{place.title}</h2>
                     <p className="text-sm mt-2">{place.description}</p>
-                  </div>
+                  </div> 
+  
+
                 </Link>
                     ))}
                     </div>
         </div>
     );
 }
+
+
+
+
